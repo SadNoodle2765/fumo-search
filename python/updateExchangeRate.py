5e8d19321e75d87352cb32a54bb318e12a191838
Warren Leu <wwleu@uci.edu>
2022-06-11 13:11:53 -0700

    Added exchange rate updater

---
+++ python/updateExchangeRate.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+from os import environ
+import requests
+
+MONGODB_URL = environ.get('MONGODB_URL')
+EXCHANGE_RATE_URL = environ.get('EXCHANGE_RATE_URL')
+
+client = MongoClient(MONGODB_URL)
+db = client.fumoApp
+
+
+def updateExchangeRate():
+    rateCollection = db.exchangeRate
+    rateCollection.drop()
+
+    rate = requests.get(EXCHANGE_RATE_URL).json()
+
+    for currency, value in rate['conversion_rates'].items():
+        item = {
+            'currency'  : currency,
+            'value'     : value
+        }
+
+        result = rateCollection.insert_one(item)
+        print(f'Added {currency} with value {value}')
+
+    print('Finished adding exchange rates')
+
+if __name__ == '__main__':
+    updateExchangeRate()
