a7608dc9ac37878041aea4d79ef8026661c17dee
Warren Leu <wwleu@uci.edu>
2022-06-10 23:14:35 -0700

    Edited backup port number

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get('/api/fumo', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
